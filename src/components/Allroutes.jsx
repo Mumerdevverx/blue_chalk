1224b773a4d0e49f6e3cc096f1f5322b5a81dea0
TalhaAsif129 <abutalha.devverx@gmail.com>
2026-08-21 18:39:04 +0500

    faet: pass new route

--- src/components/Allroutes.jsx
+++ src/components/Allroutes.jsx
@@ -6,11 +6,13 @@ import Layout from "./layout/Layout";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import News from "./pages/News";
-import NewsBlogs from "./pages/NewsBlogs";   // ✅ Dynamic component
+import NewsBlogs from "./pages/NewsBlogs"; // ✅ Dynamic component
 import Work from "./pages/Works";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfUse from "./pages/TermsOfUse";
 import AllProjectwork from "./work/AllProjectwork";
+import Workdetail from "./work/Workdetail";
+import Awardpage from "./pages/Awardpage";
 
 const Allroutes = () => {
   return (
@@ -22,17 +24,17 @@ const Allroutes = () => {
       <Route element={<Layout />}>
         <Route path="/about" element={<About />} />
         <Route path="/work" element={<Work />} />
-
         {/* ✅ NEWS ROUTES */}
-        <Route path="/news" element={<News />} />                 {/* List page */}
-        <Route path="/news/:slug" element={<NewsBlogs />} />     {/* ✅ Dynamic detail page */}
-
+        <Route path="/news" element={<News />} /> {/* List page */}
+        <Route path="/news/:slug" element={<NewsBlogs />} />{" "}
+        {/* ✅ Dynamic detail page */}
         <Route path="/contact" element={<Contact />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms-of-use" element={<TermsOfUse />} />
-
+        <Route path="/awardpage" element={<Awardpage />} />
         {/* Work Categories */}
         <Route path="/work" element={<AllProjectwork />} />
+        <Route path="/workdetail" element={<Workdetail />} />
       </Route>
     </Routes>
   );
