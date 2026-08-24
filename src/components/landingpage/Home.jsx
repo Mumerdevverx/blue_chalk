9f7e6a821ac5382c2fbc0b421c5829e4b4d5aea9
mumer.devverx@gmail.com <mumer.devverx@gmail.com>
2026-08-25 00:29:45 +0500

    feat: fix he issue Home  page

--- src/components/landingpage/Home.jsx
+++ src/components/landingpage/Home.jsx
@@ -197,7 +197,7 @@ const Home = () => {
   // ✅ LOADING STATE
   if (loading) {
     return (
-      <div className="min-h-screen flex items-center justify-center bg-white">
+      <div className="min-h-max flex items-center justify-center bg-white">
         <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-blue-600"></div>
       </div>
     );
